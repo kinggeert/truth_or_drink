a0f8a9fe455cd29d2e6ae6cfe56080960d7f29c4
Geert Bruls <geertbruls@outlook.com>
2025-03-12 13:45:15 +0100

    Added qr code scanner

--- lib/pages/join_page.dart
+++ lib/pages/join_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class JoinPage extends StatelessWidget {
   const JoinPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Join")));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Join")),
+      body: MobileScanner(
+        controller: MobileScannerController(
+          detectionSpeed: DetectionSpeed.normal,
+        ),
+        onDetect: (capture) {
+          showDialog(
+            context: context,
+            builder:
+                (context) => AlertDialog(
+                  title: const Text("You scanned this qr code:"),
+                  content: Text(capture.barcodes.first.url!.url),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'OK'),
+                      child: const Text("Ok"),
+                    ),
+                  ],
+                ),
+          );
+          HapticFeedback.vibrate();
+        },
+      ),
+    );
   }
 }
